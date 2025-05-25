e2b9fe17aef49c98ca4e6807e69a444fae772ba7
Langpeter, Lukas <lukas.langpeter@study.thws.de>
2025-05-25 16:49:49 +0200

    test route added

--- app/app.py
+++ app/app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route('/api/test', methods=['GET'])
 def test():
     return 'test was called'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
