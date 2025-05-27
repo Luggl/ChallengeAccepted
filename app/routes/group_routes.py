20b10c324b1fa6632ee06884608332e7b85216f6
Langpeter, Lukas <lukas.langpeter@study.thws.de>
2025-05-27 13:44:28 +0200

    Routes erweitert

--- app/routes/group_routes.py
+++ app/routes/group_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from services.user_service import *
 
 # Blueprint für alle Gruppenfunktionen
 group_bp = Blueprint('group', __name__)
