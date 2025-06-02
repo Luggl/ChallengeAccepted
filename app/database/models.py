aeac8dd210643c62ed23ed811925db8547f4be3a
Denis Gashi <denis.gashi@study.thws.de>
2025-06-02 10:43:08 +0200

    Tabellenerstellung und Enums

--- app/database/models.py
+++ app/database/models.py
@@ -3,6 +3,22 @@ from sqlalchemy import Column, String, Integer,Boolean, Date, ForeignKey, Table
 from sqlalchemy.dialects.sqlite import BLOB
 from sqlalchemy.orm import relationship
 from database import Base
+from enum import Enum
+from sqlalchemy import Enum as SQLEnum
+
+class StatusUnit(Enum):
+    anzahl="anzahl"
+    dauer="dauer"
+
+class Schwierigkeit(Enum):
+    easy="easy"
+    medium="medium"
+    hard="hard"
+
+class AufgabeStatus(Enum):
+    offen="offen"
+    abgeschlossen="abgeschlossen"
+    nicht_gemacht="nicht gemacht"
 
 
 class UserAchievement(Base):
@@ -46,3 +62,33 @@ class ResetToken(Base):
 
     user_id=Column(BLOB, ForeignKey("user.user_id"), unique=True)
     user=relationship("User", back_populates="token")
+
+class Membership(Base):
+    __tablename__ = "membership"
+    user_id=Column(BLOB, ForeignKey("user.user_id"), primary_key=True)
+    gruppe_id=Column(BLOB, ForeignKey("gruppe.gruppe_id"), primary_key=True)
+    isAdmin=Column(Boolean, default=False)
+
+    user=relationship("User", back_populates="membership")
+    gruppe=relationship("Gruppe", back_populates="memberships")
+class Gruppe(Base):
+    __tablename__="gruppe"
+    gruppe_id=Column(BLOB, primary_key=True, default=lambda: uuid.uuid4().bytes)
+    gruppenname=Column(String, nullable=False, unique=True)
+    beschreibung=Column(String)
+    gruppenbild=Column(String)
+    einladungscode=Column(String, nullable=False)
+    einladungscodeDatum=Column(Date)
+    erstellungsDatum=Column(Date)
+
+    memberships= relationship("Membership", back_populates="gruppe")
+    challenges=relationship("Challenge", back_populates="gruppe")
+
+class Sportart(Base):
+    __tablename__="sportart"
+
+    sportart_id=Column(BLOB, primary_key=True, default=lambda: uuid.uuid4().bytes)
+    bezeichnung= Column(String, nullable=False)
+    unit=Column(SQLEnum(StatusUnit), nullable=False)
+
+    challenges=relationship("Challenge", secondary="challenge_sportart", back_populates="sportarten")
