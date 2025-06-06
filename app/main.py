463dda9f34f1acd86511930bddf3b0b31106ee2c
Denis Gashi <denis.gashi@study.thws.de>
2025-06-06 16:46:14 +0200

    Fehler behebung

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from database.database import engine
 import database.models
-from .models import Base
-database.models.Base.metadata.create_all(bind=engine)
+from database.models import Base
+
+Base.metadata.create_all(bind=engine)
 
 print("Datenbanktabellen wurden erfolgreich erstellt.")
