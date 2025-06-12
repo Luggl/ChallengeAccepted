bf1dfeb6f744d2fa9dd0d2dfee2d62be5d4667e6
Dilan Kücük <dilan.kuecuek@study.thws.de>
2025-06-12 15:13:47 +0200

    add frontend

---
+++ app/frontend/app/src/main/java/com/example/challengeaccepted/MainActivity.kt
@@ -0,0 +1,44 @@
+// XML -> activity_main
+
+package com.example.challengeaccepted
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import androidx.core.view.WindowCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Randloses Layout aktivieren
+       // WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val registerButton = findViewById<Button>(R.id.btnStart)
+        val loginText = findViewById<TextView>(R.id.txtLogin)
+
+        /*registerButton.setOnClickListener {
+            Toast.makeText(this, "RegisterButton funktioniert!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }*/
+
+        registerButton.setOnClickListener {
+            // Weiterleitung zur Register-Seite
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginText.setOnClickListener {
+            // Weiterleitung zur Login-Seite
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/frontend/app/src/main/java/com/example/challengeaccepted/RegisterActivity.kt
@@ -0,0 +1,45 @@
+// XML -> activity_register
+
+package com.example.challengeaccepted
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Randloses Layout aktivieren
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val username = findViewById<EditText>(R.id.edit_username)
+        val email = findViewById<EditText>(R.id.edit_email)
+        val password = findViewById<EditText>(R.id.edit_password1)
+        val repeatPassword = findViewById<EditText>(R.id.edit_password2)
+        val registerButton = findViewById<Button>(R.id.button_start_regist)
+        val alreadyAccount = findViewById<TextView>(R.id.text_already_account)
+
+        registerButton.setOnClickListener {
+            // TODO: Registrierungsvorgang einbauen
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        alreadyAccount.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
