ccbb9486e5b4ee4d927074a0984b3f5d13e5d710
Stenger, Tobias <tobias.stenger@study.thws.de>
2025-06-16 10:43:35 +0200

    Register User Workflow implementiert / __init__ angepasst

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
-db = SQLAlchemy()  # außerhalb der Funktion, damit global verfügbar
+db = SQLAlchemy()
+jwt = JWTManager()  # JWTManager global verfügbar machen
 
 def create_app():
     app = Flask(__name__)
 
-    # Konfiguration (z. B. SQLite oder PostgreSQL)
+    # DB-Konfiguration
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    db.init_app(app)
+    # JWT-Konfiguration
+    app.config["JWT_SECRET_KEY"] = "supergeheimes-passwort"  # Ändere das für Produktion!
+    app.config["JWT_TOKEN_LOCATION"] = ["headers"]
+    app.config["JWT_HEADER_NAME"] = "Authorization"
+    app.config["JWT_HEADER_TYPE"] = "Bearer"
 
-    # Import und Registrierung der Routen
-    from app.routes.user_routes import user_routes
-    app.register_blueprint(user_routes)
+    db.init_app(app)
+    jwt.init_app(app)  # JWT mit App verbinden
+
+    # Blueprint registrieren
+    from app.routes.user_routes import user_bp
+    app.register_blueprint(user_bp)
 
     return app
-
-
--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL= r"sqlite:///C:\Users\TS10\PycharmProjects\gruppe-14---challenge-accepted_new\mydatabase.db"
+DATABASE_URL = "sqlite:///C:/Users/TS10/PycharmProjects/gruppe-14---challenge-accepted_new/mydatabase.db"
+
 Base=declarative_base()
 engine=create_engine(DATABASE_URL, echo=True)
 SessionLocal= sessionmaker(bind=engine)
