fd9d623f1f9b4cb75a2b7d070835b6c428c55f51
Morsells <Moritz.b.study@gmail.com>
2025-06-18 02:13:55 +0200

    dreck da

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = r"sqlite:///C:\Users\Moritz\PycharmProjects\ChallengeAccepted\gruppe-14---challenge-accepted\mydatabase.db"
+DATABASE_URL = r"sqlite:///C:\Users\Moritz\PycharmProjects\gruppe-14---challenge-accepted\mydatabase.db"
 
 Base=declarative_base()
 engine=create_engine(DATABASE_URL, echo=True)
