cde4f978456e46c8197d72a6994b4b5fa3f511dd
Langpeter, Lukas <lukas.langpeter@study.thws.de>
2025-07-08 18:13:37 +0200

    imports fixed

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from app.database.database import engine
-from app.database.models import Base
+from database.database import engine
+from database.models import Base
 
 Base.metadata.create_all(bind=engine)
 
