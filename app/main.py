9a389eb00499a0dc68516c4e9afad11178f0174c
Stenger, Tobias <tobias.stenger@study.thws.de>
2025-07-08 13:39:57 +0200

    Rollback

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from database.database import engine
-from database.models import Base
+from app.database.database import engine
+from app.database.models import Base
 
 Base.metadata.create_all(bind=engine)
 
