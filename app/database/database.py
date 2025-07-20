908a9cfc0c0f686e3f7fc1e82a884b47a7450a5e
Langpeter, Lukas <lukas.langpeter@study.thws.de>
2025-07-20 13:25:26 +0200

    SQL Echo off - Logging added in run.py

--- app/database/database.py
+++ app/database/database.py
@@ -5,7 +5,7 @@ from sqlalchemy import event
 DATABASE_URL = r"sqlite:///C:\Users\TS10\PycharmProjects\gruppe-14---challenge-accepted\mydatabase.db"
 
 Base=declarative_base()
-engine=create_engine(DATABASE_URL, echo=True)
+engine=create_engine(DATABASE_URL)
 SessionLocal= sessionmaker(bind=engine)
 
 
